fbff8a4ebdb6ee1d497e64426e840be6bb500714
xyzxlord <164474123+xyzxlord@users.noreply.github.com>
2024-06-17 23:41:02 -0700

    Add files via upload

--- alienx.lua
+++ alienx.lua
@@ -420,22 +420,22 @@ function Library:create(options)
 	}
 
 	if readfile and writefile and isfile then
-		if not isfile("MercurySettings.json") then
-			writefile("MercurySettings.json", HTTPService:JSONEncode(settings))
+		if not isfile("xalien.json") then
+			writefile("xalien.json", HTTPService:JSONEncode(settings))
 		end
-		settings = HTTPService:JSONDecode(readfile("MercurySettings.json"))
+		settings = HTTPService:JSONDecode(readfile("xalien.json"))
 		Library.CurrentTheme = Library.Themes[settings.Theme]
 		updateSettings = function(property, value)
 			settings[property] = value
-			writefile("MercurySettings.json", HTTPService:JSONEncode(settings))
+			writefile("xalien.json", HTTPService:JSONEncode(settings))
 		end
 	end
 
 	options = self:set_defaults({
-		Name = "Mercury",
+		Name = "alienx",
 		Size = UDim2.fromOffset(600, 400),
 		Theme = self.Themes[settings.Theme],
-		Link = "https://github.com/deeeity/mercury-lib"
+		Link = "alienx.cry"
 	}, options)
 
 	if getgenv and getgenv().MercuryUI then
